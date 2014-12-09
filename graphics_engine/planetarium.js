f3f55505a66e2bb955841e8f4b86d9dd1a4071ff
JakJak7 <Jakob.Bjerre@gmail.com>
2014-12-09 15:27:32 -0400

    added colored specular map, broke planetarium.js

--- graphics_engine/planetarium.js
+++ graphics_engine/planetarium.js
@@ -380,6 +380,241 @@ ViewPoint.prototype.setLookAt = function(eye, point, up) {
   mat4.mul(this._pov, this._p, this._c)
 }
 
+cameraController = {
+	eye: vec3.create(),
+	gaze: vec3.create(),
+	up: vec3.create(),
+	longitude: 0,
+	latitude: 0,
+	radius: 2,
+	gazeVector: vec3.create(),
+	targetGaze: vec3.create(),
+	hasTarget: false,
+	numClicks: 0,
+	timeFlow: 1.0,
+	timeFlow_last: 0.0,
+	
+	update: function(uea){
+		var speed = uea.realElapsed;
+		distance = Math.sqrt(Math.pow(targetGaze[0]-gazeVector[0],2)+Math.pow(targetGaze[1]-gazeVector[1],2)+Math.pow(targetGaze[2]-gazeVector[2],2));
+		if(distance < 0.02){
+			gazeVector[0] = targetGaze[0];
+			gazeVector[1] = targetGaze[1];
+			gazeVector[2] = targetGaze[2];
+			hasTarget = 0;
+			numClicks = 0;
+		}
+		else{
+			gazeVector[0] += (targetGaze[0]-gazeVector[0])*(numClicks*speed/1000);
+			gazeVector[1] += (targetGaze[1]-gazeVector[1])*(numClicks*speed/1000);
+			gazeVector[2] += (targetGaze[2]-gazeVector[2])*(numClicks*speed/1000);
+		}
+
+		radius += (targetRadius-radius)*(speed/100);
+		uea.model.setLookAt(this.eye,this.gaze,this.up)
+	}
+}
+
+function initEventHandlers(gl,n,canvas,currentAngle,u_Clicked, modelMatrix, u_ModelMatrix,hud){
+	var mouseDown = false, mouseMove = false, hasBreached = false;
+	var element = canvas;
+	var oldX, oldY;
+	element.addEventListener("mousedown", function(ev){
+		mouseDown = true;
+		mouseMove = false;
+		oldX = ev.pageX, oldY = ev.pageY;
+		//console.log(Xbefore+","+Ybefore);
+	}, false);
+	element.addEventListener("mousemove", function(ev){
+		var currentX = ev.clientX, currentY = ev.clientY;
+		var deltaX = currentX-oldX, deltaY = currentY-oldY;
+		var distMoved = Math.sqrt(Math.pow(deltaX,2)+Math.pow(deltaY,2));
+		if(mouseDown && distMoved > 5){
+			mouseMove = true;
+			hasBreached = true;
+		}
+		if(hasBreached){
+			//console.log("move, "+Math.PI_2);
+			latitude+=deltaY/100;
+			
+			// stop the latitude from wrapping around
+			if(latitude>=Math.PI_2){
+				latitude=Math.PI_2-0.001;
+			}
+			else if(latitude<=-Math.PI_2){
+				latitude=-Math.PI_2+0.001;
+			}
+			longitude+=deltaX/100;
+			oldX = currentX;
+			oldY = currentY;
+		}
+		//var deltaX = ev.clientX-Xbefore, deltaY = ev.clientY-Ybefore;
+	}, false);
+	element.addEventListener("mouseup", function(ev){
+		mouseDown = false;
+		hasBreached = false;
+		if(!mouseMove){
+			//console.log("click");
+			
+			var x = ev.clientX, y = ev.clientY;
+			//console.log("up at: "+x+","+y);
+			var rect = ev.target.getBoundingClientRect();
+			if (rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom) {
+				 // Check if it is on object
+				 var x_in_canvas = x - rect.left, y_in_canvas = rect.bottom - y;
+				 var picked = check(gl, n, x_in_canvas, y_in_canvas, currentAngle,u_Clicked, modelMatrix, u_ModelMatrix);
+				 if (picked[0] == 255){
+					  targetGaze = new Float32Array(cube1Center);
+					  if(target == 1) numClicks++;
+					  else {
+						  target = 1;
+						  numClicks = 1;
+					  }
+				  }
+				 else if (picked[1] == 255){
+					  targetGaze = new Float32Array(cube2Center);
+					  if(target == 2) numClicks++;
+					  else {
+						  target = 2;
+						  numClicks = 1;
+					  }
+				  }
+				 else if (picked[2] == 255){
+					  targetGaze = new Float32Array(cube3Center);
+					  if(target == 3) numClicks++;
+					  else {
+						  target = 3;
+						  numClicks = 1;
+					  }
+				  }
+				 else if (picked[2] == 128){
+					  targetGaze = new Float32Array(cube4Center);
+					  if(target == 4) numClicks++;
+					  else {
+						  target = 4;
+						  numClicks = 1;
+					  }
+				  }
+			 }
+		}
+		else if(mouseDown === 1){
+			//console.log("drag");
+		}
+	}, false);
+	
+	hud.onclick= function(ev){
+		var x = ev.clientX;
+		var y = ev.clientY;
+		//alert(x+","+y);
+		if(x<106){
+			// play/pause
+			if(timeFlow==0.0) {
+				timeFlow = timeFlow_last;
+				timeFlow_last = 0.0;
+			}
+			else {
+				timeFlow_last = timeFlow;
+				timeFlow = 0;
+			}
+		}
+		else if(x<166){
+			// faster
+			if (Math.abs(timeFlow)<8) timeFlow = timeFlow *2;
+		}
+		else if(x<232){
+			// slower
+			if (Math.abs(timeFlow)>0.125) timeFlow = timeFlow * 0.5;
+		}
+		else if(x<304){
+			// reverse
+		    timeFlow = timeFlow *-1;
+		}
+		else{
+			// reset
+			timeFlow = 1.0;
+		}
+	};
+	
+	// WASD free-cam (but only in the XZ plane
+	element,onkeypress = function(ev){
+		var key = String.fromCharCode(ev.charCode);
+		//console.log(key);
+		switch(key){
+		  case 'w':
+			targetGaze[0]-= Math.cos(longitude)*0.1;
+			targetGaze[2]-= Math.sin(longitude)*0.1;
+			target = 0;
+			numClicks = 1;
+			break;
+		  case 's':
+			targetGaze[0]+= Math.cos(longitude)*0.1;
+			targetGaze[2]+= Math.sin(longitude)*0.1;
+			target = 0;
+			numClicks = 1;
+			break;
+		  case 'a':
+			targetGaze[0]-= Math.sin(longitude)*0.1;
+			targetGaze[2]+= Math.cos(longitude)*0.1;
+			target = 0;
+			numClicks = 1;
+			break;
+		  case 'd':
+			targetGaze[0]+= Math.sin(longitude)*0.1;
+			targetGaze[2]-= Math.cos(longitude)*0.1;
+			target = 0;
+			numClicks = 1;
+			break;
+		  case ' ':
+			if(timeFlow==0.0) {
+				timeFlow = timeFlow_last;
+				timeFlow_last = 0.0;
+			}
+			else {
+				timeFlow_last = timeFlow;
+				timeFlow = 0;
+			}
+			break;
+		  case '1':
+		    timeFlow = 1;
+			break;
+		  case '2':
+		    timeFlow = timeFlow * 0.5;
+			break;
+		  case '3':
+		    timeFlow = timeFlow *-1;
+			break;
+		  case '4':
+			if (timeFlow<8) timeFlow = timeFlow *2;
+			break;
+		}
+    };
+    
+	// Internet Explorer, Opera, Google Chrome and Safari
+	element.addEventListener ("mousewheel", mouseScroll, false);
+	// Firefox
+	element.addEventListener("DOMMouseScroll", mouseScroll, false);
+}
+
+function mouseScroll(ev){
+	var MAXRADIUS = 20;
+	var MINRADIUS = 1;
+	targetRadius+=ev.detail/30;
+	if(targetRadius<MINRADIUS) targetRadius=MINRADIUS;
+	else if(targetRadius>MAXRADIUS) targetRadius=MAXRADIUS;
+}
+
+function check(gl, n, x, y, currentAngle, u_Clicked, viewProjMatrix, u_MvpMatrix) {
+	var picked = false;
+	gl.uniform1i(u_Clicked, 1); // Draw the cube with red
+	drawCubes(gl, n, currentAngle, viewProjMatrix, u_MvpMatrix);
+	// Read pixel at the clicked position
+	var pixels = new Uint8Array(4); // Array for storing the pixels
+	gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+	gl.uniform1i(u_Clicked, 0); // Pass false to u_Clicked: redraw cube
+	draw(gl, n, currentAngle, viewProjMatrix, u_MvpMatrix); 
+	return pixels;
+}
+
 function WebGLStart(canvasId) {
   // initialize WebGL
   var gl = WebGL.createContext(canvasId)
@@ -496,9 +731,9 @@ function WebGLStart(canvasId) {
     earthProgram.setUniform("u_PlanetPosition", bufv3)
     earthProgram.setModelViewWithNormals(planet.transform, planet.normalTransform)
     earthProgram.setDayColorTexture(planet.texture, 0)
-    if (planet.nightTexture) {
+    /*if (planet.nightTexture) {
       earthProgram.setNightColorTexture(planet.nightTexture, 1)
-    }
+    }*/
     if (planet.normalTexture) {
       vec3.transformMat3(bufv3, up, planet.normalTransform)
       earthProgram.setUniform("u_PlanetUp", bufv3)
@@ -543,6 +778,7 @@ function WebGLStart(canvasId) {
   //}
   
   var scene = createPlanetarium(gl, programs)
+  scene.addController(cameraController)
   //earthProgram.use()
   //occlusionProgram.use()
   radialBlurProgram.use().setUniform("u_TextureSize", [gl.viewportWidth, gl.viewportHeight])
